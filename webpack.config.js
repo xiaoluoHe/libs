870200733b63182a8b7ef69dcf2daa2b186a6dd8
Mingfei Ding <xiesansi@gmail.com>
2020-03-07 13:45:03 +0800

    project init

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  output: {
+    filename: "libs.js",
+    path: path.resolve(__dirname, "dist")
+  }
+};
